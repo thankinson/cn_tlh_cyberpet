b1e9395f7ff5d90bd4fd2437e975df9e7822b984
thankinson <t_hankinson@outlook.com>
2022-02-03 15:47:29 +0000

    git

--- javascript/cyberPet.js
+++ javascript/cyberPet.js
@@ -1,10 +1,16 @@
-const nameOfPet = document.getElementById(`petName`);
-const typeOfPet = document.getElementById(`petType`);
-const input = document.querySelector(`input`);
+let nameOfPet = document.getElementById(`petName`).value;
+let typeOfPet = document.getElementById(`petType`).value;
+let petCreate = document.getElementById(`createPet`);
 
-document.addEventListener(`click`, (nameOfPet) =>{
-
-
+petCreate.addEventListener(`click`, () =>{
+    let 
+    // if (typeOfPet = "cat"){
+    //     nameOfPet = new Cat;
+    // }   else if (typeOfPet = "dog") {
+    //     nameOfPet = new Dog;
+    // } else {
+    //     nameOfPet = new Fish;
+    // }
 })
 
 
@@ -49,7 +55,6 @@ class cyberPet{
             };
     }
 
-
     class Cat extends cyberPet{
         constructor(petName){
             super(petName);
@@ -78,57 +83,57 @@ class cyberPet{
         };
     };
 
-const bob = new Cat("BoB");
+// bob = new Cat("BoB");
 
-console.log(bob);
+// console.log(bob);
 
 // #################################\\
 //              test site           \\
 // #################################\\
 
-let hunger = 50;
-let play = 50;
-let thirst = 50;
-let petName = "Bob";
-const petPlay = () => {
-    play++;
-    thirst -= 5;
-    hunger -= 5;
-};
-const petHunger = () => {
-    hunger++;
-    thirst -= 5;
-    play -= 2;
-};
-const petThirst = () => {
-    thirst++;
-    hunger -= 1;
-    play -= 1;
-};
+// let hunger = 50;
+// let play = 50;
+// let thirst = 50;
+// let petName = "Bob";
+// const petPlay = () => {
+//     play++;
+//     thirst -= 5;
+//     hunger -= 5;
+// };
+// const petHunger = () => {
+//     hunger++;
+//     thirst -= 5;
+//     play -= 2;
+// };
+// const petThirst = () => {
+//     thirst++;
+//     hunger -= 1;
+//     play -= 1;
+// };
 
 
-const thePet = () => {
-    if (hunger <= 50){
-    console.log (`${petName} is hungry`);
-    } else if (thirst <= 50){
-        console.log(`${petName} is thirsty`);
-    } else if (play <= 50){
-        console.log(`${petName} wants to play`);
-    } else {
-        console.log(`${petName} is fine`);
-    }
-}
+// const thePet = () => {
+//     if (hunger <= 50){
+//     console.log (`${petName} is hungry`);
+//     } else if (thirst <= 50){
+//         console.log(`${petName} is thirsty`);
+//     } else if (play <= 50){
+//         console.log(`${petName} wants to play`);
+//     } else {
+//         console.log(`${petName} is fine`);
+//     }
+// }
 
-    const feedPet = () => {
-        for (i = 0; i <= hunger; i++){
-            petHunger();
-        } if (thirst || play == 50){
-            console.log(`${petName} os no longer hungry`);
-        }
-    }
+//     const feedPet = () => {
+//         for (i = 0; i <= hunger; i++){
+//             petHunger();
+//         } if (thirst || play == 50){
+//             console.log(`${petName} os no longer hungry`);
+//         }
+//     }
 
 
-thePet()
+// thePet()
 
 // #################################\\
 // #################################\\
@@ -138,7 +143,15 @@ thePet()
 //        the chopping block        \\
 // #################################\\
 
-
+// const WhatPet = (){
+//     if (typeOfPet = "cat"){
+//         nameOfPet = new Cat;
+//     }   else if (typeOfPet = "dog") {
+//         nameOfPet = new Dog;
+//     } else {
+//         nameOfPet = new Fish;
+//     }
+// }
 
 // #################################\\
 // #################################\\
