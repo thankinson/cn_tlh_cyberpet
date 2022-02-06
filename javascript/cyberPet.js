7d8ce824841433725fe2e955b7fa4cf7345bbc21
thankinson <t_hankinson@outlook.com>
2022-02-06 15:22:34 +0000

    updated the javascript and css and html. almost finished

--- javascript/cyberPet.js
+++ javascript/cyberPet.js
@@ -1,18 +1,84 @@
-let nameOfPet = document.getElementById(`petName`).value;
-let typeOfPet = document.getElementById(`petType`).value;
+let petPlay = document.getElementById(`petPlay`);
+let petFeed = document.getElementById(`petFeed`);
+let petDrink = document.getElementById(`petDrink`);
 let petCreate = document.getElementById(`createPet`);
 
-petCreate.addEventListener(`click`, () =>{
-    let 
-    // if (typeOfPet = "cat"){
-    //     nameOfPet = new Cat;
-    // }   else if (typeOfPet = "dog") {
-    //     nameOfPet = new Dog;
-    // } else {
-    //     nameOfPet = new Fish;
-    // }
-})
 
+let nameOfPet = document.getElementById(`petName`).value;
+let typeOfPet = document.getElementById(`petType`).value;
+// creates pet
+petCreate.addEventListener(`click`, () =>{
+  
+    console.log(nameOfPet, typeOfPet)
+    if (typeOfPet == "cat"){
+        nameOfPet = new Cat(nameOfPet);
+        console.log(nameOfPet);
+    }   else if (typeOfPet == "dog") {
+        nameOfPet = new Dog(nameOfPet);
+        console.log(nameOfPet);
+    } else {
+        nameOfPet = new Fish(nameOfPet);
+        console.log(nameOfPet);
+    };
+});
+
+
+// play with pet
+
+petPlay.addEventListener(`click`, () =>{
+
+    
+    console.log(nameOfPet.getHunger);
+    if (nameOfPet.getHunger < 1 || nameOfPet.getThirst < 1){
+            alert(`${nameOfPet.getName} your pet has died`);
+            window.location.reload();
+    } else if (nameOfPet.getPlay >= 60){
+        console.log(`${nameOfPet.getName} is tired`);
+        alert(`${nameOfPet.getName} is extreamly tired and wants to rest`)
+    } else {
+        console.log(`${nameOfPet.getName}`);
+        console.log(nameOfPet);
+        nameOfPet.playPet();
+    }
+    
+
+});
+
+petFeed.addEventListener(`click`, () =>{
+
+    // nameOfPet.petHunger();
+    console.log(nameOfPet.getHunger);
+    if (nameOfPet.getPlay < 1 || nameOfPet.getThirst < 1){
+            alert(`${nameOfPet.getName} your pet has died`);
+            window.location.reload();
+    } else if (nameOfPet.getHunger >= 60){
+        console.log(`${nameOfPet.getName} has had enogh`);
+        alert(`${nameOfPet.getName} is stuffed and dosnt want anymore food`)
+    } else {
+        console.log(`${nameOfPet.getName}`)
+        nameOfPet.petHunger();
+    }
+    
+
+});
+
+petDrink.addEventListener(`click`, () =>{
+
+    // nameOfPet.petHunger();
+    console.log(nameOfPet.getHunger);
+    if (nameOfPet.getPlay < 1 || nameOfPet.getHunger < 1){
+            alert(`${nameOfPet.getName} your pet has died`);
+            window.location.reload();
+    } else if (nameOfPet.getThirst >= 60){
+        console.log(`${nameOfPet.getName} has had enogh`);
+        alert(`${nameOfPet.getName} is no longer thirsty and dosnt want anymore water`)
+    } else {
+        console.log(`${nameOfPet.getName}`)
+        nameOfPet.petThirst();
+    }
+    
+
+});
 
 
 // ########################################################################################################
@@ -23,28 +89,28 @@ class cyberPet{
         this._petThirst = 50;
         this._petPlay = 50;
     };
-        get petName(){
+        get getName(){
             return this._petName;
         };
-        get petType(){
+        get getType(){
             return this._petType;
         };
-        get petHunger(){
+        get getHunger(){
             return this._petHunger;
         };
-        get petThirst(){
+        get getThirst(){
             return this._petThirst;
         };
-        get petPlay(){
+        get getPlay(){
             return this._petPlay;
         };
-            petPlay(){
-                this._petPlay++;
-                this._petThirst -= 5;
+            playPet(){
+                this._petPlay += 3;
+                this._petThirst -= 3;
                 this._petHunger -= 5;
             };
             petHunger(){
-                this._petHunger++;
+                this._petHunger += 2;
                 this._petThirst -= 5;
                 this._petPlay -= 2;
             };
@@ -53,12 +119,13 @@ class cyberPet{
                 this._petHunger -= 2;
                 this._petPlay -= 1;
             };
-    }
+    };
 
     class Cat extends cyberPet{
         constructor(petName){
             super(petName);
             this._petType = "Cat";
+            this._favFood = "Fish";
         };
     };
 
@@ -66,6 +133,7 @@ class cyberPet{
         constructor(petName){
             super(petName);
             this._petType = "Dog";
+            this.faveFood = "Biscuits";
         };
     };
 
@@ -73,15 +141,16 @@ class cyberPet{
         constructor(petName){
             super(petName);
             this._petType = "Fish";
+            this._favFood = "worms";
         };
     };
 
-    class Alien extends cyberPet{
-        constructor(petName){
-            super(petName);
-            this._petType = "Alien";
-        };
-    };
+    // class Alien extends cyberPet{
+    //     constructor(petName){
+    //         super(petName);
+    //         this._petType = "Alien";
+    //     };
+    // };
 
 // bob = new Cat("BoB");
 
